53db2097378914a0a8d3b439c722d5ee04b6e0e0
firja10 <fairuzfirjatullah3@gmail.com>
2022-06-20 19:46:16 +0700

    Update Project Teh Nizza Front End

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -37,11 +37,11 @@ Beranda
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Earnings (Monthly)</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                        Jumlah Barang Masuk</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">20 Buah</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                    <i class="fas fa-sign-in-alt fa-2x text-gray-300"></i>
                                 </div>
                             </div>
                         </div>
@@ -55,11 +55,11 @@ Beranda
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                        Earnings (Annual)</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                        Jumlah Barang Keluar</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">13 Buah</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                    <i class="fas fa-sign-out-alt fa-2x text-gray-300"></i>
                                 </div>
                             </div>
                         </div>
@@ -72,23 +72,17 @@ Beranda
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Pengguna
                                     </div>
                                     <div class="row no-gutters align-items-center">
                                         <div class="col-auto">
-                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">10 Orang</div>
                                         </div>
-                                        <div class="col">
-                                            <div class="progress progress-sm mr-2">
-                                                <div class="progress-bar bg-info" role="progressbar"
-                                                    style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                    aria-valuemax="100"></div>
-                                            </div>
-                                        </div>
+                                      
                                     </div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                    <i class="fas fa-user-circle fa-2x text-gray-300"></i>
                                 </div>
                             </div>
                         </div>
@@ -102,11 +96,11 @@ Beranda
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                        Pending Requests</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                        Jumlah Kategori Barang</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">18 Jenis</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                    <i class="fas fa-list-alt fa-2x text-gray-300"></i>
                                 </div>
                             </div>
                         </div>
