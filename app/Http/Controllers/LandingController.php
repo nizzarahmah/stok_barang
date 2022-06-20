9948c16fb971efdae540ad413790379ed2fa369f
firja10 <fairuzfirjatullah3@gmail.com>
2022-06-20 17:55:26 +0700

    Update Projek 1

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LandingController extends Controller
+{
+    //
+
+
+    public function index()
+    {
+        # code...
+
+        return view('admin.home');
+
+    }
+
+
+
+    
+
+
+
+}
