8a93d4b4c33fe43e65d78f073e7180ccded833f1
firja10 <fairuzfirjatullah3@gmail.com>
2022-08-18 20:34:39 +0700

    Hapus Kelompok Pengguna

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -150,7 +150,7 @@
                         <div class="bg-white py-2 collapse-inner rounded">
                             {{-- <h6 class="collapse-header">Custom Components:</h6> --}}
                             <a class="collapse-item" href="{{url('/data_user')}}">Pengguna</a>
-                            <a class="collapse-item" href="{{url('/kelompok_user')}}">Kelompok Pengguna</a>
+                            {{-- <a class="collapse-item" href="{{url('/kelompok_user')}}">Kelompok Pengguna</a> --}}
                         </div>
                     </div>
                 </li>
